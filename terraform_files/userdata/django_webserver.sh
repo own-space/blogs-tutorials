b95fa4ba5c5817fbdbf99c121cee2dfaf46b8ce4
Er-sumit <41416816+Er-sumit@users.noreply.github.com>
2022-08-03 13:17:47 +0100

    Update django_webserver.sh

--- terraform_files/userdata/django_webserver.sh
+++ terraform_files/userdata/django_webserver.sh
@@ -1,9 +1,7 @@
-cd /src/application/
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 /src/application/manage.py makemigrations && python3 /src/application/manage.py migrate
+nohup python3 manage.py runserver 0.0.0.0:4000 > /var/log/django_app.log 2>&1 &
 sed -i 's/DB_NAME/PRDB_NAME/g' /src/application/django_app/.env
 echo 'DB_NAME="bloglocalv1"' >> /src/application/django_app/.env
 python3 /src/application/manage.py dumpdata auth > /src/application/initial_data_auth.json
 cp /src/application/django_app/env /src/application/django_app/.env
 python3 /src/application/manage.py loaddata /src/application/initial_data_auth.json
-nohup python3 manage.py runserver 0.0.0.0:4000 > /var/log/django_app.log 2>&1 &
